448a32dacba9716161492706425bf8f14229726b
e-rojas <ejrojas@icloud.com>
2020-06-04 15:35:24 -0300

    tsconfi file update

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  constructor() {
+    this.templateElement = document.querySelector(
+      "#project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.querySelector("#app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
