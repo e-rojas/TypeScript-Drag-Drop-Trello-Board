e9541539470e09e8a5b6effa39523ce1ca22aa51
e-rojas <ejrojas@icloud.com>
2020-06-05 08:11:39 -0300

    project list class created

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,35 @@ interface Validatble {
   min?: number;
   max?: number;
 }
+//project list class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.querySelector(
+      "#project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.querySelector("#app2") as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
 
 function validate(validatableInput: Validatble) {
   let isValid = true;
@@ -142,3 +171,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
